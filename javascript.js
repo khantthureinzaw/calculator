cf05d69b0eff0fdd391e9c9ab1cb6531d9d5bfa1
Albert <102751941+khantthureinzaw@users.noreply.github.com>
2024-12-14 14:53:38 +0700

    Add keyboard functionality

--- javascript.js
+++ javascript.js
@@ -24,58 +24,44 @@ const display = document.querySelector('.display');
 const firstNumDisplay = document.querySelector('#firstNum');
 const secondNumDisplay = document.querySelector('#secNum');
 const operatorDisplay = document.querySelector('#operatorDisplay');
-firstNumDisplay.innerText = '0'
+firstNumDisplay.innerText = '0';
 
-const numbers = document.querySelectorAll('.numbers');
-numbers.forEach(number => {
-    number.addEventListener('click', () => {
-        if (operatorDisplay.innerText === '') {
-            if (firstNumDisplay.innerText === '0') firstNumDisplay.innerText = '';
-            firstNumDisplay.innerText += number.querySelector('button').value;
-        } else {
-            secondNumDisplay.innerText += number.querySelector('button').value;
-        }
+function handleNumberInput(value) {
+    if (operatorDisplay.innerText === '') {
+        if (firstNumDisplay.innerText === '0') firstNumDisplay.innerText = '';
+        firstNumDisplay.innerText += value;
+    } else {
+        secondNumDisplay.innerText += value;
+    }
+}
 
-    })
-});
+function handleOperatorInput(value) {
+    if (secondNumDisplay.innerText) {
+        firstNumDisplay.innerText = operate(Number(firstNumDisplay.innerText), operatorDisplay.innerText.trim(), Number(secondNumDisplay.innerText));
+        operatorDisplay.innerText = '';
+        secondNumDisplay.innerText = '';
+    }
+    let operator = value;
+    operatorDisplay.innerText = ` ${operator} `;
+}
 
-const operators = document.querySelectorAll('.operators');
-operators.forEach(op => {
-    op.addEventListener('click', () => {
-        if (secondNumDisplay.innerText) {
-            firstNumDisplay.innerText = operate(Number(firstNumDisplay.innerText), operatorDisplay.innerText.trim(), Number(secondNumDisplay.innerText));
-            operatorDisplay.innerText = '';
-            secondNumDisplay.innerText = '';
-        }
-        let operator = op.querySelector('button').value;
-        operatorDisplay.innerText = ` ${operator} `;
-    })
-});
-
-const decimal = document.querySelector('#decimal');
-decimal.addEventListener('click', () => {
+function addDecimal() {
     if (secondNumDisplay.innerText && !secondNumDisplay.innerText.includes('.')) {
         secondNumDisplay.innerText = secondNumDisplay.innerText + '.';
     } else if (!operatorDisplay.innerText && !firstNumDisplay.innerText.includes('.')) {
         firstNumDisplay.innerText = firstNumDisplay.innerText + '.';
     }
-});
+}
 
-const percent = document.querySelector('#percentage');
-percent.addEventListener('click', () => {
+function addPercent() {
     if (secondNumDisplay.innerText) {
         secondNumDisplay.innerText = Number(secondNumDisplay.innerText) / 100;
     } else if (!operatorDisplay.innerText) {
         firstNumDisplay.innerText = Number(firstNumDisplay.innerText) / 100;
     }
-});
+}
 
-let firstOperand = 0;
-let secondOperand;
-let operator = '';
-
-const equal = document.querySelector('#equal');
-equal.addEventListener('click', () => {
+function equation() {
     let result;
     firstOperand = Number(firstNumDisplay.innerText);
     secondOperand = Number(secondNumDisplay.innerText);
@@ -89,17 +75,15 @@ equal.addEventListener('click', () => {
     firstNumDisplay.innerText = result;
     secondNumDisplay.innerText = '';
     operatorDisplay.innerText = '';
-})
+}
 
-const clearBtn = document.querySelector('.clear');
-clearBtn.addEventListener('click', () => {
+function clearDisplay() {
     firstNumDisplay.innerText = '0';
     secondNumDisplay.innerText = '';
     operatorDisplay.innerText = '';
-});
+}
 
-const deleteBtn = document.querySelector('.delete');
-deleteBtn.addEventListener('click', () => {
+function backspace() {
     if (secondNumDisplay.innerText) {
         secondNumDisplay.innerText = secondNumDisplay.innerText.slice(0, -1);
     } else if (operatorDisplay.innerText) {
@@ -110,7 +94,63 @@ deleteBtn.addEventListener('click', () => {
             firstNumDisplay.innerText = '0';
         }
     }
+}
+
+//Button events
+const numbers = document.querySelectorAll('.numbers');
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        handleNumberInput(number.querySelector('button').value);
+    })
 });
+
+const operators = document.querySelectorAll('.operators');
+operators.forEach(op => {
+    op.addEventListener('click', () => {
+        handleOperatorInput(op.querySelector('button').value);
+    })
+});
+
+const decimal = document.querySelector('#decimal');
+decimal.addEventListener('click', addDecimal);
+
+const percent = document.querySelector('#percentage');
+percent.addEventListener('click', addPercent);
+
+let firstOperand = 0;
+let secondOperand;
+let operator = '';
+
+const equal = document.querySelector('#equal');
+equal.addEventListener('click', equation)
+
+const clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', clearDisplay);
+
+const deleteBtn = document.querySelector('.delete');
+deleteBtn.addEventListener('click', backspace);
+
+
+//Keyboard events
+document.addEventListener('keydown', (e) => {
+    const key = e.key;
+
+    if (key >= 0 && key <= 9) {
+        handleNumberInput(key);
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        handleOperatorInput(key);
+    } else if (key === '.') {
+        addDecimal();
+    } else if (key === '%') {
+        addPercent();
+    } else if (key === 'Enter') {
+        equation();
+    } else if (key === 'Backspace') {
+        backspace();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    }
+})
 
 function operate(x, operator, y) {
     switch (operator) {
